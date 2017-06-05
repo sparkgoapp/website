7c07533ccce0eafa8f69a93d6221eae86722dc0e
e24032112 <e24032112@yahoo.com.tw>
2017-06-05 20:41:43 +0800

    Git fix

--- sparkgo/js/landing.js
+++ sparkgo/js/landing.js
@@ -1,40 +1,7 @@
-var item = { type : 0, SID : string };
 
-$.ajax({
-        type:'POST',
-        accepts:'json',
-        url:'njs/sub.njs',
-        data:{"type":item.type,"SID":item.SID},
-        success: function(sub){
-                               $.ajax({
-                                      type:'POST',
-                                      accepts:'json',
-                                      url:'njs/api.njs',
-                                      data : {"type": item.type,"sub" : sub },
-                                      success: function(data){
-                                                        $.each(data, function(index,item){
-                                                                        var DOM = $("<li><div><div class="circle"></div><p class="name"></p><img class="thunder"/><p class="time"></p><p class="date"></p></div><div class="pic"><div class="middle"><img class="mainpic"/><img class="heart"/><img class="platform"/><p class="number"></p></div><img class="message"/><p class="text"></p></div></li>");
-                                                                        var ok=render[item.type](DOM, item.info);
-                                                               });
-                                      },
-                                      error: function(){
-                                                        console.log("feed fails");
-                                      }
-                                      
-                               }); 
-        },
-        error: function(){
-                          console.log("sub fails");
-        }
-});
-
-$('.heart').mouseover(function(){
-                            $('div.middle img:eq(0)').css("display","none");
-                            $('div.middle img:eq(1)').css("display","block");
-                            });
-
-
-$('.heart').mouseout(function(){
-                           $('div.middle img:eq(0)').css("display","block");
-                           $('div.middle img:eq(1)').css("display","none");
-                           });
+$(document).ready(function(){
+                  $('.heart').click(function(){
+                                    $('.heart').css("display","none");
+                                    $('.active-heart').css("display","block");
+                                    });
+                  });
