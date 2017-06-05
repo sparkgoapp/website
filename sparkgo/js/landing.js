faf034ddccf4b2abba724ebb81320c9d8cc7fd26
e24032112 <e24032112@yahoo.com.tw>
2017-06-05 21:57:38 +0800

    landing.js store

--- sparkgo/js/landing.js
+++ sparkgo/js/landing.js
@@ -1,7 +1,20 @@
+$(document).ready(function(){
+                  $("li").click(function(){
+                                var newurl = "1m-00-singlePost.html";
+                                var obj = parseInt($(this).id.substring(2));
+                                window.localStorage.setItem(page,posts[obj]);
+                                document.location.href = newurl;
+                  })
+                  
+});
 
 $(document).ready(function(){
                   $('.heart').click(function(){
                                     $('.heart').css("display","none");
                                     $('.active-heart').css("display","block");
-                                    });
                   });
+                  $('.active-heart').click(function(){
+                                    $('.heart').css("display","block");
+                                    $('.active-heart').css("display","none");
+                  });
+});
