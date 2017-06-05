2e2a1fe2e8ec0dcc9587d4be2b0f7a25391fea3e
e24032112 <e24032112@yahoo.com.tw>
2017-06-05 23:02:26 +0800

    change heart to function

--- sparkgo/js/landing.js
+++ sparkgo/js/landing.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
                   $("li").click(function(){
                                 var newurl = "1m-00-singlePost.html";
-                                var obj = parseInt($(this).id.substring(2));
+                                var obj = parseInt($(this).attr("id").substring(2));
                                 window.localStorage.setItem(page,posts[obj]);
                                 document.location.href = newurl;
                   })
                   
 });
 
-$(document).ready(function(){
-                  $('.heart').click(function(){
-                                    $('.heart').css("display","none");
-                                    $('.active-heart').css("display","block");
-                  });
-                  $('.active-heart').click(function(){
-                                    $('.heart').css("display","block");
-                                    $('.active-heart').css("display","none");
-                  });
-});
+
+function heart() {
+    $('.heart').click(function(){
+                      $(this).css("display","none");
+                      $(this).parent().find(".active-heart").css("display","block");
+                      });
+    $('.active-heart').click(function(){
+                             $('.heart').css("display","block");
+                             $('.active-heart').css("display","none");
+                             });
