f5e52a0f1678efe93b06e96aad58d9d64b402185
e24032112 <e24032112@yahoo.com.tw>
2017-06-17 20:35:55 +0800

    Search

--- sparkgo/js/search.js
+++ sparkgo/js/search.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
                   if(window.localStorage.fromwhere == "landing"){
                     $("#keyword").val() = window.localStorage.keyword;
+                    $.ajax({
+                         url:'https://luffy.ee.ncku.edu.tw/~fad11204/test/js/login.njs',
+                         method: 'POST',
+                         data:{m_u:$("#keyword").val()},
+                         success: function(data){
+                         $.each(str.items, function(index,item){
+                                $().html();
+                                });
+                         
+                         },
+                         error: function(){
+                         $().html("Sorry, there is nothing matched");
+                         }
+                         
+                         });
                   }
                   });
 
@@ -21,3 +36,4 @@ $('.search').keydown(function() {
                             
                             });
                      });
+});
